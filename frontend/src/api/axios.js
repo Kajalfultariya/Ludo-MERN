1a2d82ccaa317a02eeb7ad1a422ffbe34b7a16ff
kajalfulatriya <Kajlfutariya@gmail.com>
2026-08-05 21:23:23 +0530

    Update axios.js

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5176/api",
+  baseURL: import.meta.env.VITE_API_URL || "http://ludo-game-eta-one.vercel.app/api",
 });
 
 api.interceptors.request.use((config) => {
