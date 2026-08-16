82384caf11205fba74fe7dfa04da80e39f77a005
kajalfulatriya <Kajlfutariya@gmail.com>
2026-08-16 23:12:42 +0530

    url set

--- backend/server.js
+++ backend/server.js
@@ -8,12 +8,24 @@ import gameRoutes from "./routes/gameRoutes.js";
 dotenv.config();
 const app = express();
 
-app.use(cors({
-    origin: process.env.CLIENT_ORIGIN,
-    credentials: true,
-  }));
+  app.use(
+    cors({
+        origin: [
+            "http://localhost:5176",
+            "https://ludo-game-eta-one.vercel.app",
+        ],
+        methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+        allowedHeaders: ["Content-Type", "Authorization"],
+        credentials: true,
+    })
+);
 app.use(express.json());
-
+app.get("/", (req, res) => {
+  res.status(200).json({
+    success: true,
+    message: "MERN backend is running",
+  });
+});
 app.get("/api/health", (req, res) => res.json({ status: "ok" }));
 app.use("/api/auth", authRoutes);
 app.use("/api/game", gameRoutes);
